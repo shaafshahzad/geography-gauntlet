2a5f9ee61c52edbb953f8cb670336a53bc05e04a
Shaaf <shaaf.m.shahzad@gmail.com>
2024-05-03 04:24:23 -0400

    Question generation complete

--- src/app/gauntlet/page.tsx
+++ src/app/gauntlet/page.tsx
@@ -1,7 +1,13 @@
+import { generateQuestion } from "~/lib/utils/generate-question";
+
 export default async function Gauntlet() {
+  const question = await generateQuestion();
+
   return (
     <div>
       <h1>gauntlet</h1>
+      <p>Question: {question.question}</p>
+      <p>Template: {question.template}</p>
     </div>
   );
 }
--- src/lib/utils/generate-question.ts
+++ src/lib/utils/generate-question.ts
@@ -12,7 +12,7 @@ export async function generateQuestion() {
   }
 
   // get a random question template
-  const templateId = 1;
+  const templateId = Math.floor(Math.random() * 8) + 1;
   const templateResponse = await api.gauntlet.getQuestion({
     question_id: templateId,
   });
@@ -23,18 +23,40 @@ export async function generateQuestion() {
 
   let question;
   switch (templateData.question_id) {
-    case 1:
-    case 9:
+    case 1: // capital of country
+    case 8: // country of capital
       question = templateData.template
         .replace("{country}", country.name)
         .replace("{capital}", country.capital);
       break;
+    case 2: // country with some letters
+      const value = Math.floor(Math.random() * 8) + 4;
+      question = templateData.template.replace("{value}", value.toString());
+      break;
+    case 3: // country starting with letter
+    case 4: // country ending with letter
+      const letter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+      question = templateData.template.replace("{letter}", letter);
+      break;
+    case 5: // country with color in flag
+      const colors = ["red", "green", "blue", "yellow", "black", "white"];
+      const color = colors[Math.floor(Math.random() * colors.length)];
+      question = templateData.template.replace("{color}", color || ""); // TO FIX (color is not defined in the template)
+      break;
+    case 6: // population less than
+    case 7: // population more than
+      const population =
+        Math.round((Math.floor(Math.random() * 100000000) + 5000000) / 100000) *
+        100000;
+      const formattedPopulation = population.toLocaleString();
+      question = templateData.template.replace("{value}", formattedPopulation);
+      break;
+    default:
+      throw new Error("Invalid question template ID.");
   }
 
-  // replace the placeholders in the template with the actual data
-
   return {
-    question, // e.g., "What is the capital of Italy?"
-    country, // The country object fetched from the database
+    question,
+    template: templateData.template,
   };
 }
