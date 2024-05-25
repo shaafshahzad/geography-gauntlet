75312fc4644ab60dbec33ad7fa6afbbbe77b2087
Shaaf <shaaf.m.shahzad@gmail.com>
2024-05-25 15:14:47 -0400

    Build issues

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,22 +27,6 @@ const config = {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
 
-      keyframes: {
-        wave: {
-          "0%": { transform: "rotate(0.0deg)" },
-          "10%": { transform: "rotate(14deg)" },
-          "20%": { transform: "rotate(-8deg)" },
-          "30%": { transform: "rotate(14deg)" },
-          "40%": { transform: "rotate(-4deg)" },
-          "50%": { transform: "rotate(10.0deg)" },
-          "60%": { transform: "rotate(0.0deg)" },
-          "100%": { transform: "rotate(0.0deg)" },
-        },
-      },
-      animation: {
-        wave: "wave 1s ease-in-out infinite",
-      },
-
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
