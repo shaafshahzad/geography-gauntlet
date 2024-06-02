18d5107e8d38fe6e7e1688ce55f760def479bb75
Shaaf <shaaf.m.shahzad@gmail.com>
2024-06-02 04:23:51 -0400

    Build issues pt2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,10 @@ const config = {
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/prefer-nullish-coalescing": "warn",
     "@typescript-eslint/prefer-for-of": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    " @typescript-eslint/no-unsafe-return": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
