47ab3fe51613e58f694f0b7c5f212d3ebda79beb
Shaaf <shaaf.m.shahzad@gmail.com>
2024-06-04 09:55:43 -0400

    Styled main page

--- src/app/_components/main/gauntlet.tsx
+++ src/app/_components/main/gauntlet.tsx
@@ -3,6 +3,7 @@
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardTitle } from "~/components/ui/card";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 import { globeConfig, sampleArcs } from "~/lib/Globe";
 import dynamic from "next/dynamic";
 
@@ -20,11 +21,29 @@ export function Gauntlet() {
     <div className="flex h-full w-full flex-row gap-5">
       <Card className="flex h-full w-1/2 flex-col gap-4 p-5">
         <CardTitle>Gauntlet</CardTitle>
-        <CardContent className="flex h-full w-full flex-col justify-between gap-11 pb-0">
-          <p>something something idk</p>
-          <Button onClick={() => router.push("/gauntlet")}>
-            Start Gauntlet
-          </Button>
+        <CardContent className="flex h-full w-full flex-col justify-between gap-4 pb-0">
+          <p className="text-2xl font-medium italic">
+            Test your geography knowledge under pressure!
+          </p>
+          <div className="flex flex-col items-center justify-center gap-4">
+            <p className="text-md">
+              In Gauntlet, you have <u>10 seconds</u> to answer each question
+              correctly.
+              <br />
+              Score points by answering quickly, but beware – once time runs
+              out, it's <b>game over</b>.
+            </p>
+            <Image
+              src="/gauntlet-icon.png"
+              width={150}
+              height={150}
+              alt="Gauntlet icon"
+              className="align-middle"
+            />
+            <Button className="w-full" onClick={() => router.push("/gauntlet")}>
+              Start Gauntlet
+            </Button>
+          </div>
         </CardContent>
       </Card>
       <div className="flex w-1/2 flex-col items-center justify-center">
--- src/app/_components/main/quizzes.tsx
+++ src/app/_components/main/quizzes.tsx
@@ -3,6 +3,7 @@
 import { Card, CardTitle, CardContent } from "~/components/ui/card";
 import { Button } from "~/components/ui/button";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export function Quizzes() {
   const router = useRouter();
@@ -12,7 +13,23 @@ export function Quizzes() {
       <CardTitle>Quizzes</CardTitle>
       <CardContent className="flex h-full justify-between gap-[98px] pb-0">
         <div className="flex w-1/2 flex-col justify-between">
-          <p>Click to start a country quiz</p>
+          <div className="flex flex-row">
+            <div className="flex flex-col gap-4">
+              <p className="text-xl font-medium italic">
+                How well do you know the world?
+              </p>
+              <p className="text-md">
+                Name all the countries in the world within the time limit.
+              </p>
+            </div>
+            <Image
+              src="/country-quiz-icon.png"
+              width={125}
+              height={15}
+              alt="World Quiz icon"
+              className="ml-6 align-middle"
+            />
+          </div>
           <Button
             onClick={() => router.push("/country-quiz")}
             className="w-full"
@@ -21,7 +38,23 @@ export function Quizzes() {
           </Button>
         </div>
         <div className="flex w-1/2 flex-col justify-between">
-          <p>Click to start a flag quiz</p>
+          <div className="flex flex-row">
+            <div className="flex flex-col gap-4">
+              <p className="text-xl font-medium italic">
+                How well do you know world flags?
+              </p>
+              <p className="text-md">
+                Identify the country each flag belongs to within the time limit.
+              </p>
+            </div>
+            <Image
+              src="/flag-quiz-icon.png"
+              width={125}
+              height={15}
+              alt="World Quiz icon"
+              className="ml-6 align-middle"
+            />
+          </div>
           <Button onClick={() => router.push("/flag-quiz")} className="w-full">
             Start Flag Quiz
           </Button>
