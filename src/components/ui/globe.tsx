a5dabe60746aeca00b6b9a4bbfb3c8a4497b1554
Shaaf <shaaf.m.shahzad@gmail.com>
2024-06-19 05:49:56 -0400

    Build issues

--- src/components/ui/globe.tsx
+++ src/components/ui/globe.tsx
@@ -11,7 +11,7 @@ const SIZE = 350;
 function isWebGLContext() {
   const canvas = document.createElement("canvas");
   const gl =
-    canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    canvas.getContext("webgl") ?? canvas.getContext("experimental-webgl");
 
   return gl instanceof WebGLRenderingContext;
 }
