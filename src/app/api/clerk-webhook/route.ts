ef35b256c61c747dc55aac24e2e30444e5153be0
Shaaf <shaaf.m.shahzad@gmail.com>
2024-06-23 10:11:14 -0400

    Build issues pt4

--- src/app/api/clerk-webhook/route.ts
+++ src/app/api/clerk-webhook/route.ts
@@ -1,38 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
+import { object } from "zod";
 import { api } from "~/trpc/server";
 
 interface Body {
-  data?: {
-    deleted?: boolean;
-    id?: string;
-  };
-  type?: string;
-}
-
-interface UserDeletedEvent {
   data: {
     deleted: boolean;
     id: string;
+    object: string;
   };
-  type: "user.deleted";
-}
-
-function isUserDeletedEvent(body: any): body is UserDeletedEvent {
-  return (
-    body &&
-    body.data &&
-    typeof body.data.deleted === "boolean" &&
-    typeof body.data.id === "string" &&
-    body.type === "user.deleted"
-  );
+  event_attributes: {
+    http_request: {
+      client_ip: string;
+      user_agent: string;
+    };
+  };
+  object: string;
+  type: string;
 }
 
 export async function POST(req: NextRequest) {
   try {
-    const body = await req.json();
+    const body: Body = await req.json();
 
-    if (isUserDeletedEvent(body)) {
-      const userId = body.data.id;
+    if (body.data?.deleted && body.type === "user.deleted") {
+      const userId = body.data.id!;
 
       await api.user.deleteUser({ user_id: userId });
 
